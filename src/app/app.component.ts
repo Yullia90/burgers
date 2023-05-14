86264cbd35c9a64f2feaa5de2ab9c9f241669611
Yullia90 <y.tsarenko@harwind.com.ua>
2023-05-15 00:42:13 +0300

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'burgers';
+  form = this.fb.group({
+    order: ['', Validators.required],
+    name: ['', Validators.required],
+    phone: ['', Validators.required],
+  });
+
+  productsData = [
+    {
+image:
+    }
+  ]
+
+  constructor(private fb: FormBuilder) {}
+  scrollTo(target: HTMLElement) {
+    target.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  confirmOrder() {
+    if (this.form.valid) {
+      alert('Спасибо за заказ!');
+      this.form.reset();
+    }
+  }
 }
