510bb041790ed28f56b5a4f5434fa672fb4e8ae2
Yullia90 <y.tsarenko@harwind.com.ua>
2023-05-15 12:24:08 +0300

    add Type

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,8 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  currency = '$';
+
   form = this.fb.group({
     order: ['', Validators.required],
     name: ['', Validators.required],
@@ -15,13 +17,111 @@ export class AppComponent {
 
   productsData = [
     {
-image:
-    }
-  ]
+      image: 'Burger1.png',
+      title: 'Бургер чеддер & бекон',
+      text: 'Котлета из говядины криспи, булочка, томат, сыр Чеддер, грудинка, лук красный, салат айсбер, майонез, кетчуп, сырный соус',
+      price: 8,
+      basePrice: 8,
+      grams: 360,
+    },
+    {
+      image: 'Burger2.png',
+      title: 'BBQ с беконом и курицей',
+      text: 'Булочка бриошь с кунжутом, куриная котлета, сыр чеддер, томат, огурец маринованный, лук маринованный, салат Ромен, бекон, соус BBQ',
+      price: 7,
+      basePrice: 7,
+      grams: 390,
+    },
+    {
+      image: 'Burger3.png',
+      title: 'Дабл биф бургер',
+      text: 'Две говяжьи котлеты, сыр чеддер, салат романо, маринованные огурцы, свежий томат, бекон, красный лук, соус бургер, горчица',
+      price: 10,
+      basePrice: 10,
+      grams: 420,
+    },
+    {
+      image: 'Burger4.png',
+      title: 'Баварский бургер',
+      text: 'Булочка для бургера, говяжья котлета, красный лук, сыр, охотничья колбаска, соус барбекю, соус сырный, салат айсберг',
+      price: 7,
+      basePrice: 7,
+      grams: 220,
+    },
+    {
+      image: 'Burger5.png',
+      title: 'Бекон чизбургер',
+      text: 'Булочка для бургера, говяжья котлета, грудинка, помидор, огурец маринованный, сыр, сырный соус, кетчуп, зелень',
+      price: 8,
+      basePrice: 8,
+      grams: 220,
+    },
+    {
+      image: 'Burger6.png',
+      title: 'Индиана бургер',
+      text: 'Булочка для бургера, котлета куриная, грудинка, яйцо, огурец маринованный, криспи лук, кетчуп, соус сырный, горчица, зелень',
+      price: 9,
+      basePrice: 9,
+      grams: 320,
+    },
+    {
+      image: '7.png',
+      title: 'Вегги бургер',
+      text: 'Булочка для бургера, вегетарианская котлета, красный лук, сыр, свежий томат, соусбарбекю, соус сырный, салат айсберг',
+      price: 8,
+      basePrice: 8,
+      grams: 280,
+    },
+    {
+      image: '8.png',
+      title: 'Плаксивый Джо',
+      text: 'Булочка для бургера, говяжья котлета, грудинка, помидор, огурец маринованный, красный лук, сыр, перец халапеньо, кетчуп, зелень',
+      price: 7,
+      basePrice: 7,
+      grams: 380,
+    },
+    {
+      image: '9.png',
+      title: 'Двойной чиз бургер',
+      text: 'Булочка для бургера, две говяжьи котлеты, двойной сыр чеддар, огурец маринованный, криспи лук, кетчуп, соус сырный, горчица, зелень',
+      price: 11,
+      basePrice: 11,
+      grams: 400,
+    },
+    {
+      image: '10.png',
+      title: 'Фрешбургер',
+      text: 'Булочка для бургера, говяжья котлета, бекон, сыр чеддар, яйцо, салями, соус барбекю, соус сырный, салат айсберг, свежий томат',
+      price: 9,
+      basePrice: 9,
+      grams: 300,
+    },
+    {
+      image: '11.png',
+      title: 'Цуккини бургер',
+      text: 'Булочка для бургера, вегетарианская котлета из нута, цуккини на гриле, помидор, огурец маринованный, сыр, горчичный соус, кетчуп, зелень',
+      price: 8,
+      basePrice: 8,
+      grams: 320,
+    },
+    {
+      image: '12.png',
+      title: 'Двойной бургер чеддар',
+      text: 'Булочка для бургера, котлета говяжья, грудинка, красный лук, огурец маринованный, томат, кетчуп, двойной сыр чеддар, горчица, зелень',
+      price: 9,
+      basePrice: 9,
+      grams: 360,
+    },
+  ];
 
   constructor(private fb: FormBuilder) {}
-  scrollTo(target: HTMLElement) {
+  scrollTo(target: HTMLElement, burger?: any) {
     target.scrollIntoView({ behavior: 'smooth' });
+    if (burger) {
+      this.form.patchValue({
+        order: burger.title + ' (' + burger.price + ' ' + this.currency + ')',
+      });
+    }
   }
 
   confirmOrder() {
@@ -29,5 +129,29 @@ image:
       alert('Спасибо за заказ!');
       this.form.reset();
     }
+  }
+
+  changeCurrency() {
+    let newCurrency = '$';
+    let coefficient = 1;
+    // будем определять какая сейчас валюта
+    if (this.currency === '$') {
+      newCurrency = '₴';
+      coefficient = 38;
+    } else if (this.currency === '₴') {
+      newCurrency = '€';
+      coefficient = 3;
+    } else if (this.currency === '€') {
+      newCurrency = '¥';
+      coefficient = 6.9;
+    } else if (this.currency === '¥') {
+      newCurrency = '£';
+      coefficient = 0.9;
+    }
+    this.currency = newCurrency;
+
+    this.productsData.forEach((item) => {
+      item.price = +(item.basePrice * coefficient).toFixed(1);
+    });
   }
 }
